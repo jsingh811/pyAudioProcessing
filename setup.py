f6a5f917c49993f45aab237719ff6c0d111c73c9
Jyotika Singh <jyotika811@gmail.com>
2021-03-19 16:45:40 -0700

    Update setup

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 import setuptools
 
+# The text of the README file
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setuptools.setup(
    name='pyAudioProcessing',
-   version='1.0',
+   version='1.1.0',
    description='Audio processing-feature extraction and building machine learning models from audio data.',
+   long_description=long_description,
+   long_description_content_type="text/markdown",
    author='Jyotika Singh',
-   packages=['pyAudioProcessing'],
+   packages=setuptools.find_packages(),#['pyAudioProcessing'],
    url="https://github.com/jsingh811/pyAudioProcessing",
+   include_package_data=True,
+   python_requires='>=3.6',
+   py_modules=["pyAudioProcessing"],
    classifiers=[
        "Programming Language :: Python :: 3",
        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
