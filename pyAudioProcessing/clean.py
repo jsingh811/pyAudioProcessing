d7b361c5ec4d918400533c1a497496c45812d460
Jyotika Singh <jyotika811@gmail.com>
2021-07-20 14:28:15 -0700

    Add audio cleaning logic

---
+++ pyAudioProcessing/clean.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 10 15:23:55 2021
+
+@author: jsingh
+"""
+# Imports
+import os
+import numpy
+from scipy.io import wavfile
+from pyAudioProcessing.utils import read_audio
+
+# Globals
+LOW_PT_THRESHOLD = 0.01
+
+# Functions
+
+def remove_silence(
+    input_file, output_file="without_sil.wav", thr=LOW_PT_THRESHOLD
+):
+    """
+    Remove silences from input audio file.
+    """
+    sampling_rate, signal = read_audio(input_file)
+    signal = numpy.array(
+        [
+            list(i)
+            for i in signal
+            if not numpy.all((i == 0))
+        ]
+    )
+    mean_signal = [numpy.mean(list(i)) for i in signal]
+    thrs_p = thr * max(mean_signal)
+    thrs_n = thr * min(mean_signal)
+    signal = numpy.array(
+        [
+            list(i)
+            for i in signal
+            if numpy.all((i > thrs_p)) or numpy.all((i < thrs_n))
+        ]
+    )
+    wavfile.write(output_file, sampling_rate, signal)
+
+
